595e9abe3ca050c2ed69e45bc0e2617e758d6e08
NaserAldeen <30509@aum.edu.kw>
2019-08-20 14:19:10 +0300

    NaserAldeen Naser - Mariam Haitham

--- restaurants/views.py
+++ restaurants/views.py
@@ -7,6 +7,13 @@ def restaurant_list(request):
 
     context = {
 
+    "my_list": [
+    {"restaurant_name": "KFC", "food_type": "Fast food" },
+    {"restaurant_name": "McDonald's", "food_type": "Better fast food" },
+    {"restaurant_name": "Hardees", "food_type": "Not so good fast food" },
+
+    ],
+
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +21,9 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+        "my_object":{
+            "restaurant_name": "KFC",
+            "food_type": "Fast food",
+        }
     }
     return render(request, 'detail.html', context)
